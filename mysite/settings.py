a89afb38a4bb59637b6d87f8c32f1d4259132545
Bemah <bridgettebemah@gmail.com>
2024-12-04 09:29:18 -1200

    My-first-blog, fifth commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,10 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-  BASE_DIR / 'icons'
-]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]  # Path to your app-specific static files
+
+STATIC_ROOT = BASE_DIR / "staticfiles"  # Directory where collected static files will be stored
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
